93a0c46b0d31d3db5099c9bc13efaffe1f1ce949
Veronika <veron9399bog@gmail.com>
2021-08-24 20:37:53 +0300

    feat: Do L25.js

---
+++ js/L25.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//! sort
+
+const arr = [1, 14, 4, 30, 54],
+      sorted = arr.sort(compareNum);
+
+function compareNum(a,b) {
+    return a - b;
+}
+
+console.log(sorted);
+
+
+// debugger;
